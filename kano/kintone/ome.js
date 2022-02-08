7859a3005dd0b16f89df0f432d11438f1119b2bd
unknown <ichiro.kano2@asahi.email.ne.jp>
2022-02-08 20:34:23 +0900

    kintone

--- kano/kintone/ome.js
+++ kano/kintone/ome.js
@@ -1,25 +1,11 @@
 /*
  this is for kintone on NPO Ome Kodomomirai.
 */
-(function(){
+(function () {
     "use strict";
-    kintone.events.on(['app.record.create.show','app.record.edit.show'], function(event) {
-        // スペースIDを取得する
-        const spaceID = kintone.app.record.getSpaceElement('kanoSpace');
-        // ボタンを作成する
-        const btn = document.createElement('button');
-        btn.textContent = 'かのボタンrev5';
-        spaceID.appendChild(btn);
-
+    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function (event){
         var record = event.record;
-        record['独自関数'].value = "あたい";
-        record['独自関数'].disabled = true;
+        record['独自関数'].value = "あああ";
 
-        //btn.onclick=()=>{
-            //let record = kintone.app.record.get();
-            //let kanoValue = record.record['チーム'].value;
-            //event.record.record['独自関数'].value = 'だよ';
-        //}
         return event;
-    });
-})();
+    });})();
