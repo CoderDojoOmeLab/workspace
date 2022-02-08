501e3947acfe5154eb2dec8a31b53bb98630bbc8
unknown <ichiro.kano2@asahi.email.ne.jp>
2022-02-08 20:51:38 +0900

    kintone

--- kano/kintone/ome.js
+++ kano/kintone/ome.js
@@ -4,6 +4,15 @@
 (function () {
     "use strict";
     kintone.events.on(['app.record.create.submit', 'app.record.edit.submit'], function (event){
+
+        // スペースIDを取得する
+        const spaceID = kintone.app.record.getSpaceElement('kanoSpace');
+        // ボタンを作成する
+        const btn = document.createElement('button');
+        btn.textContent = 'かのボタンrev5';
+        spaceID.appendChild(btn);
+
+        // フィールドに値を書く
         var record = event.record;
         record['myFunc'].value = "Yeah!";
 
